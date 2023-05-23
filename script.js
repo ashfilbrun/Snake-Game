b4368f13fca3d9853713f0525a8081db51c9cbbd
ashfilbrun <ashesclaire@gmail.com>
2023-05-23 15:08:06 -0700

    make snake head and food appear

--- script.js
+++ script.js
@@ -1,21 +1,35 @@
-// define variables
-let sqrSize = 25;
-let rows = 20;
-let columns = 20;
-let board;
+const 
+let foodX;
+let foodY;
+let gameOver = false;
 let context;
-let canvas = document.getElementById('snake')
-let canvas2d = canvas.getContext('2d')
+let snakeX
+let snakeY
+let speedX = 0;
+let speedY = 0;
+let snakeBody = [];
+
 // game functionality
 
-window.onload = function() {
-  board = document.getElementById('board');
-  board.height = rows * sqrSize;
-  board.width = columns * sqrSize;
-  context = board.getContext('2d');
-
-  update();
+function initializeVar() {
+  snakeBody = [[0, 0]];
+  food = 0;
+  direction = 'right';
+  canMove = false;
+  isPlaying = false;
 }
+
+// const changeFoodPosition = () => {}
+
+// window.onload = function() {
+//   board = document.getElementById('board');
+//   board.height = rows * sqrSize;
+//   board.width = columns * sqrSize;
+//   context = board.getContext('2d');
+
+//   update();
+// }
+
 // render 
 
 // win conditions
